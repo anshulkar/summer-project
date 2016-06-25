9a6978c26b5be6c93bc6795eb1b43cc95bb7d3e2
Varun Joglekar <joglekar.varun@gmail.com>
2016-06-25 19:53:08 +0530

    Created a utility class
    Functions included are::
    app initialisation function
    product add function
    product check
    login
    signup.

---
+++ app/src/main/java/summer/utk/com/summer/utility.java
@@ -0,0 +1,140 @@
+package com.example.varun.baasboxapp;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.baasbox.android.*;
+import com.baasbox.android.json.JsonObject;
+
+import java.util.List;
+
+public class utility {
+
+    private BaasBox client;
+    static boolean flag;
+    static boolean loginFlag=false;
+    static boolean signUpFlag=false;
+    static String Username="",Pwd="";
+    static BaasUser user;
+    static boolean result=false;
+
+    //Call this in onCreate method of the very first activity of the app
+    void init(Context appContext){
+        BaasBox.Builder b =
+                new BaasBox.Builder(appContext);
+        client = b.setApiDomain("40.121.94.107")
+                .setAppCode("1234567890")
+                .init();
+    }
+
+    //For checking whether the barcode exists in the db
+    boolean checkProduct(String s)
+    {
+
+        final BaasQuery PREPARED_QUERY =
+                BaasQuery.builder()
+                        .collection("sample")
+                        .where("jar ='"+s+"' and _author='admin'")
+                        .build();
+        PREPARED_QUERY.query(new BaasHandler<List<JsonObject>>(){
+            @Override
+            public void handle(BaasResult<List<JsonObject>> res){
+                if(res.isSuccess())
+                {
+                    //Toast.makeText(getApplicationContext(),res.value().toString(),Toast.LENGTH_LONG).show();
+                    //JsonObject obj=res.value();
+                    List<JsonObject> objlist=res.value();
+                    result=true;
+                    Log.d("Product Check","No. of items "+objlist.size());
+                }
+                else {
+                    result=false;
+                    Log.d("Product Check","Error:: "+res.value().toString());
+
+                }
+
+
+            }
+        });
+        return result;
+    }
+
+
+    //To insert new Product
+    public boolean insert(String name,String latitude,String longitude)
+    {
+
+
+        BaasDocument doc = new BaasDocument("sample");
+
+        doc.put("Product_Name",name).put("Latitude",latitude).put("Longitude",longitude);
+        Log.d("Insert Function",doc.toJson().toString());
+        doc.save(new BaasHandler<BaasDocument>() {
+            @Override
+            public void handle(BaasResult<BaasDocument> res) {
+                if(res.isSuccess()) {
+                    Log.d("Insert Function","Reached here");
+                    flag=true;
+                } else {
+                    try {
+                        Log.d("Insert Function",res.get().toString());
+                        flag=false;
+                    } catch (BaasException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+        return flag;
+
+    }
+
+    public boolean signUp(String U,String P)
+    {
+
+        user = BaasUser.withUserName(U)
+                .setPassword(P);
+
+        user.signup(new BaasHandler<BaasUser>(){
+            @Override
+            public void handle(BaasResult<BaasUser> result){
+                if(result.isSuccess()) {
+                    signUpFlag=true;
+                } else {
+                    Log.e("SIGNUP Function","Show error",result.error());
+                }
+            }
+        });
+        return signUpFlag;
+    }
+
+    public void login(String U,String P)
+    {
+        Username=U;
+        Pwd=P;
+        BaasUser user = BaasUser.withUserName(U)
+                .setPassword(P);
+
+        user.login(new BaasHandler<BaasUser>() {
+            @Override
+            public void handle(BaasResult<BaasUser> result) {
+                if(result.isSuccess()) {
+                    Log.d("LOGIN",result.value().toString());
+                    loginFlag=true;
+
+
+                    //Success code here
+                } else {
+                    Log.e("LOGIN","Show error",result.error());
+
+                }
+
+            }
+        });
+
+
+    }
+}
